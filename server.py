0504d6d8c70ac13459e29b18d821036976b24d96
mahlakoane <mahlakoane.shaun@gmail.com>
2020-05-17 14:21:31 +0200

    Server one

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #IPV4 and TCP
+host = socket.gethostname()
+port = 9999
+
+#Establish server interface at port 9999 of this machine 
+sock.bind((host, port))
+
+sock.listen(2)
+
+print('Waiting for client connection...')
+
+
+#Accept client connection here
+myclient, clientAddress = sock.accept()
+clientMsg= myclient.recv(1024)   #Buffer size.
+
+
+print(clientMsg.decode("utf-8"))
+
+
+sock.close()
