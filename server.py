ff89618b68de85cc81d2b46ba41bd89e69b65ac0
mahlakoane <mahlakoane.shaun@gmail.com>
2020-05-19 11:06:41 +0200

    UDP server working

--- server.py
+++ server.py
@@ -1,33 +1,25 @@
 import socket
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #IPV4 and TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = socket.gethostname()
 port = 9999
 
 #Establish server interface at port 9999 of this machine 
 sock.bind((host, port))
 
-sock.listen(2)
-
-print('Waiting for client connection...')
-
-#Accept client connection here
-
-myclient, clientAddress = sock.accept()
-print('connection established!')
-
-
-
+print('waiting for UDP messages...')
 
 while True:
 
- clientMsg = myclient.recv(1024)   #Buffer size.
+ result = sock.recvfrom(1024)   #Buffer size.
+ clientMsg=result[0]
+ clientAddress=result[1]
  decodedMsg = clientMsg.decode("utf-8")
  decodedMsgUpper = decodedMsg.upper() 
        
  if decodedMsgUpper=="EXIT":
 
-        myclient.sendall(decodedMsgUpper.encode("utf-8"))
+        sock.sendto(decodedMsgUpper.encode("utf-8"),clientAddress)
         break
 
  else:
